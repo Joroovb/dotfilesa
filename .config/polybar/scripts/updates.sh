f9ba9d1b7c7d28da4b8fbf49bd8f8af5717e24b9
Joroovb <jorisvanbreugel@gmail.com>
2020-11-28 13:03:32 +0100

    changed ugly icons to prettier ones

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
 
@@ -25,11 +25,11 @@ while true; do
     # every 10 seconds another check for updates is done
     while (( UPDATES > 0 )); do
         if (( UPDATES == 1 )); then
-            echo "$UPDATES Update"
+            echo "  $UPDATES Update"
         elif (( UPDATES > 1 )); then
-            echo "$UPDATES Updates"
+            echo "  $UPDATES Updates"
         else
-            echo " "
+            echo " "
         fi
         sleep 10
         get_total_updates
@@ -38,7 +38,7 @@ while true; do
     # when no updates are available, use a longer loop, this saves on CPU
     # and network uptime, only checking once every 30 min for new updates
     while (( UPDATES == 0 )); do
-        echo " "
+        echo " "
         sleep 1800
         get_total_updates
     done
