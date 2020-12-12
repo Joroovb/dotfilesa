f3731719bff8eca0766755d2b474058b5ab13ca6
Joroovb <jorisvanbreugel@gmail.com>
2020-12-12 16:04:24 +0100

    added ncspot

--- .config/polybar/scripts/spotify.sh
+++ .config/polybar/scripts/spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 main() {
-  if ! pgrep -x spotify >/dev/null; then
+  if ! pgrep -x ncspot >/dev/null; then
     echo ""; exit
   fi  
 
@@ -9,7 +9,7 @@ main() {
   domain="org.mpris.MediaPlayer2"
   path="/org/mpris/MediaPlayer2"
 
-  meta=$(dbus-send --print-reply --dest=${domain}.spotify \
+  meta=$(dbus-send --print-reply --dest=${domain}.ncspot \
     /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
 
   artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
